a5a3a3b006bfa6ab2c4fc30d5a418c782c8749ce
sfliu <sfliu@Ctrip.com>
2016-11-02 18:28:16 +0800

    code split

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Router, browserHistory } from 'react-router';
+import rootRoute from './pages/routes';
 
 import store from './stores'
 import { Provider } from 'react-redux'
@@ -13,13 +15,17 @@ import { Provider } from 'react-redux'
 import App from './pages/App';
 
 // Render the main component into the dom
-// ReactDOM.render(<App />, document.getElementById('app'));
-ReactDOM.render(
+// ReactDOM.render(<App />, document.getElementById('app'));  //for react only
+	
+ReactDOM.render(											  //for redux
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('app')
 )
 
-
+/*ReactDOM.render(											   //for react-router and history
+  <Router history={browserHistory} routes={rootRoute}/>,
+  document.getElementById('root')
+);*/
 
