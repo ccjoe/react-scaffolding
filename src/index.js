5d3ed9acba89dfeef50a65c79dfc9fe028562aea
sfliu <sfliu@Ctrip.com>
2016-11-03 18:14:30 +0800

    router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Router, browserHistory } from 'react-router';
+import {Router, Route, browserHistory} from 'react-router';
 import rootRoute from './pages/routes';
 
 import store from './stores'
@@ -13,19 +13,24 @@ import { Provider } from 'react-redux'
 
 // import App from './components/Main';
 import App from './pages/App';
+import Battle from './pages/Battle';
 
 // Render the main component into the dom
 // ReactDOM.render(<App />, document.getElementById('app'));  //for react only
 	
-ReactDOM.render(											  //for redux
+/*ReactDOM.render(											  //for redux
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('app')
-)
+)*/
 
-/*ReactDOM.render(											   //for react-router and history
-  <Router history={browserHistory} routes={rootRoute}/>,
-  document.getElementById('root')
-);*/
+ReactDOM.render((<Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}/>
+	  <Route path="/battle" component={Battle}/>
+	</Router>
+  </Provider>),
+    document.getElementById('app')
+);
 
--- src/pages/App.js
+++ src/pages/App.js
@@ -8,9 +8,28 @@ import Header from '../components/Header'
 import MainSection from '../components/MainSection'
 import * as TodoActions from '../actions'
 
-const App = ({todos, remote, actions}) =>  (<div>
-    <Header addTodo={actions.addTodo} onFetch={actions.fetchTodos}/>
-    <MainSection todos={todos} remote={remote} actions={actions} />
+import { browserHistory,Link } from 'react-router'
+
+const navTo = function (e) {
+    e.preventDefault()
+    const path = '/battle'
+    browserHistory.push(path)
+}
+const Nav = <div className="">
+              <h2>React Router Tutorial</h2>
+              <ul className="todo-list" role="nav">
+                <li><Link to="/" activeClassName="active">Home</Link></li>
+                <li><Link to="/battle" activeClassName="active"  onClick={navTo}>Battle</Link></li>
+              </ul>
+            </div>
+
+const App = ({todos, remote, actions}) =>  (
+  <div>
+    <div>
+      <Header addTodo={actions.addTodo} onFetch={actions.fetchTodos}/>
+      <MainSection todos={todos} remote={remote} actions={actions} />
+    </div>
+     {Nav}
   </div>
 )
 
